faa28f17601b21da89165cb0547ffb1262417b9f
Lexi Harrop <lharrop@lcs.on.ca>
2023-11-03 09:54:25 -0400

    Created CircleView as a helper to make ContentView shorter.

---
+++ Stopwatch/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  Stopwatch
+//
+//  Created by Lexi on 2023-11-03.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
