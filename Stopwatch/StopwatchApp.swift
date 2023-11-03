5c498c232a8c2feb82cb37b734e9455ba93a11ca
Lexi Harrop <lharrop@lcs.on.ca>
2023-11-02 23:17:39 -0400

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Lexi on 2023-11-02.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
